2f2d38e90a8a18e7326b73f72c6fd6317be6be54
Olga <origanna@yandex.ru>
2025-02-14 04:23:38 +0300

    Add Sem2:Task5

---
+++ Sem2/Task5/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход координаты точки X и Y, причём X ≠ 0 и Y ≠ 0, и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+// Пример:
+// X = 2; Y = 3 => 1
+// X = -5; Y = 3 => 2
+// X = -3; Y = -2 => 3
+// X = 4; Y = -2 => 4
+
+Console.WriteLine("Введите X: ");
+int X = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y: ");
+int Y = Convert.ToInt32(Console.ReadLine());
+
+if (X > 0 && Y > 0)
+{
+    Console.WriteLine("Координатная четверть плоскости: 1");
+}
+else if (X < 0 && Y > 0)
+{
+    Console.WriteLine("Координатная четверть плоскости: 2");
+}
+else if (X < 0 && Y < 0)
+{
+    Console.WriteLine("Координатная четверть плоскости: 3");
+}
+else if (X > 0 && Y < 0)
+{
+    Console.WriteLine("Координатная четверть плоскости: 4");
+}
+else
+{
+    Console.WriteLine("Некорректные координаты!");
+}
