acebf9b673aca1d221e813b90b262aac0653b089
Olga <origanna@yandex.ru>
2025-02-16 17:40:41 +0300

    Add Lesson2:Task3

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// int n = 10;
+int[] array = { 2, 5, 60, 7, 8, 35, 1, 3, 250, 5 };
+int i = 0;
+
+while (i < array.Length)
+{
+    if (array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+}
