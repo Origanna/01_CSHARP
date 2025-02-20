6190ee0851de88d6df3d38d27c0309865ae968e3
Olga <origanna@yandex.ru>
2025-02-20 21:43:27 +0300

    Add Sem3:Task2 and Task3

--- Sem3/Task2/Program.cs
+++ Sem3/Task2/Program.cs
@@ -4,7 +4,7 @@
 // [1 -5 6]
 // => [-1 5 -6]
 
-int[] array = new int[10]{-3, 4, -5, 6, 8, -4, 7, 9, -1, 3};
+int[] array = new int[10] { -3, 4, -5, 6, 8, -4, 7, 9, -1, 3 };
 
 for (int i = 0; i < array.Length; i++)
 {
