a67d9ca85fcf1befbb7479501526e0e513a3c8ca
Olga <origanna@yandex.ru>
2025-02-25 14:54:58 +0300

    Add Sem6:Task5

---
+++ Sem6/Task5/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте строку, состоящую из слов, разделённых пробелами. Сформировать строку, в которой слова расположены в обратном порядке. В полученной строке слова должны быть также разделены пробелами.
+// Пример:
+// "Hello my world" => "world my Hello"
+
+string ReverseWords(string[] strNewSplit)
+{
+    string result = "";
+
+    for (int i = strNewSplit.Length - 1; i >= 0; i--)
+    {
+        result += strNewSplit[i] + " ";
+    }
+
+    return result;
+}
+
+string strNew = "Hello my world";
+string[] strNewSplit = strNew.Split(); // ["Hello", "my", "world"]
+string result = ReverseWords(strNewSplit);
+Console.WriteLine(result);
